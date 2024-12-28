b68582b25adb9505fd622436c1a20b68d998a014
Aris Kyprianidis <ariskyprian@outlook.com>
2024-12-28 16:56:35 +0200

    Encoder preprocessing

--- encoder.py
+++ encoder.py
@@ -1,9 +1,21 @@
 import numpy as np
+from scipy.signal import lfilter
 
 
 def RPE_frame_st_coder(s0: np.ndarray):
+    
     #Preprocessing
-    s_preprocessed = pre_emphasis(offset_compensation(s0))
+    alpha = 32735 * pow(2,-15)
+    beta = 28180 * pow(2,-15)
+
+    # Offset Compensation: y[k] = x[k] - x[k-1] + alpha * y[k-1]
+    b1 = [1, -1]  # Numerator coefficients
+    a1 = [1, -alpha]  # Denominator coefficients
+    offset_compensated = lfilter(b1, a1, s0)
+    # Pre-emphasis: y[k] = x[k] - beta * x[k-1]
+    b2 = [1, -beta]  # Numerator coefficients
+    a2 = [1]  # Denominator coefficients
+    pre_emphasized = lfilter(b2, a2, offset_compensated)
 
     #Short-term analysis
     ak = compute_predictor_coefficients(s_preprocessed) 
